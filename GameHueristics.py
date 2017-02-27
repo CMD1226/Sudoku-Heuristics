090f7b409a3a91aeb2a4b6698d22a447e00df46d
BVirdell <bradenvirdell@gmail.com>
2017-02-26 18:10:41 -0700

    Update and rename README.md to GameHueristics.py

---
+++ GameHueristics.py
@@ -0,0 +1,22 @@
+"# Sudoku Heuristics" 
+
+__Author__ = 'Collin Miller, Braden Virdell'
+__Contact__ = 'cmille16@uccs.edu, bvirdell@uccs.edu'
+__CopyRight__ = ''
+__license__ = ''
+__date__ = ''
+__version__ = '1.0'
+
+
+#====================================================================
+# Import Statements
+#====================================================================
+
+import numpy as np
+import copy as cp 
+import random as rd
+#====================================================================
+# Classes and Methods
+#====================================================================
+
+
